22fc99aa28bea0ed779a576f85e2ff7bb7887bcd
He Zhang <zhanghecool@outlook.com>
2020-11-13 18:32:44 +0800

    feat(component): 增加基础文件

---
+++ src/config.js
@@ -0,0 +1,33 @@
+/* eslint-disable max-len */
+
+if (process.env.BROWSER) {
+  throw new Error(
+    'Do not import `config.js` from inside the client-side code.'
+  );
+}
+
+module.exports = {
+  // Node.js app
+  port: process.env.PORT || 3000,
+
+  // API Gateway
+  api: {
+    // API URL to be used in the client-side code
+    clientUrl: process.env.API_CLIENT_URL || '',
+    // API URL to be used in the server-side code
+    serverUrl:
+      process.env.API_SERVER_URL ||
+      `http://localhost:${process.env.PORT || 3000}`,
+  },
+
+  // Web analytics
+  analytics: {
+    // https://analytics.google.com/
+    googleTrackingId: process.env.GOOGLE_TRACKING_ID, // UA-XXXXX-X
+  },
+
+  // Authentication
+  auth: {
+    jwt: { secret: process.env.JWT_SECRET || 'Crane Framework' },
+  },
+};
